e205bc7b8372d2d92e2d753f5c53e5a8740d05bd
mbblp <mbb-mail@gmx.de>
2017-01-21 15:52:22 +0100

    add paper lamps

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ core.register_node("lamps:lamp_wood", {
 		"default_wood.png^lamps_overlay.png",
 		"default_wood.png^lamps_overlay.png"
 	},
-	groups = {choppy = 3, oddly_breakable_by_hand = 2,attached_node = 1},
+	groups = {choppy = 3, oddly_breakable_by_hand = 2,},
 	drawtype = "nodebox",
 	light_source = 11,
 	paramtype = "light",
@@ -103,6 +103,77 @@ core.register_node("lamps:track_lamp", {
 	}
 })
 
+core.register_node("lamps:lamp_steel_wall", {
+	description = "steel wall Lamp",
+	tiles = {
+		"lamps_track_end_side.png",
+		"lamps_track_end_side.png",
+		"lamps_track_end_side.png^lamps_overlay_wall2.png",
+		"lamps_track_end_side.png^lamps_overlay_wall1.png",
+		"lamps_track_end_side.png^lamps_overlay.png",
+		"lamps_track_end_side.png^lamps_overlay.png"
+	},
+	groups = {choppy = 3, oddly_breakable_by_hand = 2,},
+	drawtype = "nodebox",
+	light_source = 15,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.5, -0.125, 0.0625, -0.375, 0.375},
+			{-0.0625, -0.5, -0.1875, 0.0625, -0.25, -0.0625},
+			{-0.0625, -0.375, -0.25, 0.0625, -0.125, -0.125},
+			{-0.125, -0.125, -0.3125, 0.125, 0.5, -0.0625},
+			{-0.1875, -0.125, -0.375, -0.125, 0.5, -0.3125},
+			{-0.1875, -0.125, -0.0625, -0.125, 0.5, -3.72529e-09},
+			{-0.1875, -0.125, -0.375, 0.1875, -0.0625, -4.47035e-08},
+			{0.125, -0.125, -0.0625, 0.1875, 0.5, -1.11759e-08},
+			{0.125, -0.125, -0.375, 0.1875, 0.5, -0.3125},
+			{-0.1875, 0.4375, -0.375, 0.1875, 0.5, -8.9407e-08},
+			{-0.0625, -0.4375, 0.375, 0.0625, -0.125, 0.5},
+			{-0.0625, -0.5, 0.3125, 0.0625, -0.3125, 0.4375},
+		}
+	}
+})
+
+core.register_node("lamps:paper_lamp", {
+	description = "Paper Lamp",
+	tiles = {
+		"lamps_paper.png",
+
+	},
+	groups = {choppy = 3, oddly_breakable_by_hand = 2,},
+	drawtype = "nodebox",
+	light_source = 15,
+	paramtype = "light",
+
+
+})
+
+core.register_node("lamps:paper_lamp_small", {
+	description = "Small Paper Lamp",
+	tiles = {
+		"lamps_paper_top_1.png",
+		"lamps_paper_top_2.png",
+		"lamps_paper_small.png",
+		"lamps_paper_small_2.png",
+		"lamps_paper_small_front.png",
+		"lamps_paper_small_front.png"
+	},
+	groups = {choppy = 3, oddly_breakable_by_hand = 2,},
+	drawtype = "nodebox",
+	light_source = 15,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.25, 0, 0.25, 0.25, 0.5},
+		}
+	}
+})
+
 core.register_node("lamps:metal_lantern", {
 	description = 'Metal Lantern',
 	tiles = {
@@ -169,10 +240,34 @@ core.register_craft({
 })
 
 core.register_craft({
-	output = 'lamps:metal_lantern',
+	output = "lamps:metal_lantern",
 	recipe = {
 		{'', 'default:steel_ingot', ''},
 		{'default:glass', 'default:torch', 'default:glass'},
 		{'dye:black', 'default:steel_ingot', 'dye:black'},
+	}
+})
+
+core.register_craft({
+	output = "lamps:paper_lamp",
+	recipe = {
+		{'default:paper', 'group:stick', 'default:paper'},
+		{'group:stick', 'default:torch', 'group:stick'},
+		{'default:paper', 'group:stick', 'default:paper'},
+	}
+})
+
+core.register_craft({
+	output = "lamps:paper_lamp_small 8",
+	recipe = {
+		{"lamps:paper_lamp"}
+	}
+})
+
+core.register_craft({
+	output = "lamps:lamp_steel_wall",
+	recipe = {
+		{"", "lamps:metal_lantern"},
+		{"default:steel_ingot", ""},
 	}
 })
